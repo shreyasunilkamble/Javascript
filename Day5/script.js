3a31e413590880ee773e8b146f54ae73bb570981
shreyasunilkamble <shreyakamblebhor@gmail.com>
2025-04-17 22:20:59 +0530

    added assignment or comparison operators

---
+++ Day5/script.js
@@ -0,0 +1,84 @@
+//Assignment operators
+
+let a=10
+// console.log(a)
+
+let x=2
+x+=6   //x=x+6
+// console.log(x)
+
+let b=5
+b-=2   //b=b-2
+// console.log(b)
+
+let p=7
+p*=5   //p=p*5
+// console.log(p)
+
+let q=4
+q/=2   //q=q/2
+// console.log(q)
+
+let r=8
+r%=5   //r=r%5
+// console.log(r)
+
+
+//Comparison Operator
+
+
+//Double eual(==):
+ let m=2
+ let n=5
+// console.log(m==n)
+
+ let f=6
+ let g=6
+// console.log(f==g)
+
+
+//Not equal(!=):
+ let u=10
+ let v=10
+//  console.log(u!=v)
+
+ let a=5
+ let b=3
+//  console.log(a!=b)
+
+
+//Strict equal / Triple equal to(===):
+
+let OTP = 1234
+let USER_OTP = "1234"
+// console.log(OTP === USER_OTP)
+
+//Strict not equal(!==):
+
+let ab=3
+let cd=4
+// console.log(ab!==cd)
+
+//Greater than(>):
+ 
+let mn=50
+let pq=40
+// console.log(mn>pq)
+
+
+//Less than(<):
+ 
+let vw=5
+let yz=4
+// console.log(vw<yz)
+
+//Greater than or equal to(>=):
+ 
+let result = 12 > 12   //false
+let result1 = 12 >= 12  //true
+// console.log(result)
+
+//Less than or equal to(<=):
+
+let res = 12 < 12   //false
+let res1 = 12 <= 12  //true
