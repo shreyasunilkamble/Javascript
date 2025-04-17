78801470afd8e08c1344397a1d1f04cd020de9bc
shreyasunilkamble <shreyakamblebhor@gmail.com>
2025-04-17 13:02:54 +0530

    add console & operators

---
+++ Day4/script.js
@@ -0,0 +1,18 @@
+let message = "Hellow world"
+
+console.log(message)
+
+let err_message = "something went wrong"
+
+console.error(err_message)
+
+let warn_message = "failed to fetch"
+
+console.warn(warn_message)
+
+let info = "learn js with easycode"
+
+console.log(info)
+
+
+// console.clear()
