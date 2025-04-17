6cf04514361f57bb6d3369946e2b37a810675593
shreyasunilkamble <shreyakamblebhor@gmail.com>
2025-04-17 12:33:36 +0530

    added data types

---
+++ Day3/script.js
@@ -0,0 +1,42 @@
+//Primitive
+
+//Number
+
+let PI = 3.14
+let pincode = 412206
+let radius = 5
+
+
+//String
+
+let college_name = "ATC BHOR"
+let city_name = Pune 
+let email_id = "shreyakamblebhor@gmail.com"
+
+
+//Boolean
+// true or false
+
+let LOGINED = true
+let BAN = false
+
+
+//Null
+
+ let a = null
+
+
+ //Undefined 
+  let b
+
+
+  //Non-primitive
+
+  let obj = {
+    name: "shreya"
+    city: "pune"
+  }
+
+  let greet = ["hellow", "good", "day"]
+
+ 
