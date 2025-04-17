accb82e81070e192553e558644d9f9b3835ff7e1
shreyasunilkamble <shreyakamblebhor@gmail.com>
2025-04-17 23:21:38 +0530

    add global object

---
+++ Day7/script.js
@@ -0,0 +1,7 @@
+let myHeading = "<h1>learn javascript with easy code</h1>"
+
+let subHeading = "<p>we have task to complete javascript in 30 days and these is day 7</p>"
+
+document.writeln(myHeading)
+
+document.writeln(subHeading)
