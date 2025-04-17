1f81a9e00729250c047198fbcda71478f8a2ca40
shreyasunilkamble <shreyakamblebhor@gmail.com>
2025-04-17 23:00:05 +0530

    add logical operators

---
+++ Day6/script.js
@@ -0,0 +1,34 @@
+// let age = 19;
+
+// let result = age >= 18 ? "able to vote" : "not able to vote";
+
+// console.log(result)
+
+
+// let BAN_VALUE = false
+
+// let res = BAN_VALUE == true ? "you are ban" : "you are not ban"
+
+// console.log(res)
+
+
+// let myNumber = 0
+
+// let result = myNumber > 0 ? "positive" : "negative"
+
+// console.log(result)
+
+
+// let num1 = 112
+
+// let result = num1 % 2 == 0 ? "even" : "odd"
+
+// console.log(result)
+
+
+let role = "admin"
+
+let resultDashboard = role == "user" ? "user dashboard" : "admin dashboard"
+
+console.log(resultDashboard)
+
